82e82949bce0d975172079f16a3febdebb88b7bc
charlieolinsky <charlesolinsky@gmail.com>
2024-04-01 23:23:22 -0400

    Added Env Vars to Application

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,19 @@
-package cmd
+package main
 
 /*
 	Entry Point for App
 */
+
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+func main(){
+	//Load vars from .env into the application environment
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error Loading .env file")
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/charlieolinsky/my-blog
 
 go 1.22.1
+
+require github.com/joho/godotenv v1.5.1
