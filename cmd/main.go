ed0fd4591b874747e3061d9eb6c7bc597dbb4f7f
charlieolinsky <charlesolinsky@gmail.com>
2024-04-07 20:20:18 -0400

    Basic Tests for Create and Get

--- cmd/main.go
+++ cmd/main.go
@@ -7,9 +7,12 @@ package main
 */
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"os"
 
+	"github.com/charlieolinsky/my-blog/internal/repo"
 	"github.com/charlieolinsky/my-blog/pkg/sqlite"
 	"github.com/joho/godotenv"
 	_ "github.com/mattn/go-sqlite3"
@@ -26,10 +29,39 @@ func main() {
 	dbPath := os.Getenv("DB_PATH")
 
 	//Initialize Database
-	db, err := sqlite.InitDataBase(dbPath)
+	db, err := sqlite.InitDataBase(dbPath + "test.db")
 	if err != nil {
 		log.Fatalf("Error Initializing Database -- %v", err)
 	}
 	defer db.Close() //Close when main() finishes execution
+
+	/* DAL TESTS */
+
+	//Create User
+	userRepo := repo.NewUserRepository(db)
+
+	newUser := repo.User{
+		Role:              "admin",
+		Email:             "newuser@blog.com",
+		Password:          "hey123",
+		FirstName:         "Charles",
+		LastName:          "Olinsky",
+		ProfilePictureUrl: "",
+		CreatedAt:         "now",
+		UpdatedAt:         "now",
+	}
+
+	err = userRepo.CreateUser(context.Background(), newUser)
+	if err != nil {
+		log.Fatalf("DAL Failed to Create User -- %v", err)
+	}
+
+	//Get User
+	user, err := userRepo.GetUser(context.Background(), 1)
+	if err != nil {
+		log.Fatalf("DAL Failed to Get User -- %v", err)
+	} else {
+		fmt.Printf("Got User: %s%s%s", user.FirstName, " ", user.LastName)
+	}
 
 }
